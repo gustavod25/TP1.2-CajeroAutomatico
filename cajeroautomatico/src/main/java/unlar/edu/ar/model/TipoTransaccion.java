7fb7014c6169314060e2095a3b39a722e3870de7
riveroeduardo223 <eduardorivero821@gmail.com>
2026-04-23 15:22:29 -0300

    transaccion

---
+++ cajeroautomatico/src/main/java/unlar/edu/ar/model/TipoTransaccion.java
@@ -0,0 +1,7 @@
+package unlar.edu.ar.model;
+
+public enum TipoTransaccion {
+
+    DEPÓSITO, EXTRACCIÓN, TRANSFERENCIA, CONSULTA
+
+}
---
+++ cajeroautomatico/src/main/java/unlar/edu/ar/model/Transaccion.java
@@ -0,0 +1,17 @@
+package unlar.edu.ar.model;
+
+public class Transaccion {
+
+    
+
+
+ tipo: ENUM (DEPÓSITO, EXTRACCIÓN, TRANSFERENCIA, CONSULTA)
+
+
+
+
+    
+}
+
+
+
